f7d98ea06cbb054f065e511f953983e3e9c9360d
dasha <rus.dar.ni@gmail.com>
2024-07-16 22:20:32 +0300

    multiply.js

---
+++ 8kyu/square-sum.js
@@ -0,0 +1,17 @@
+// DESCRIPTION:
+//    Complete the square sum function so that it squares each number passed into it and then sums the results together.
+
+
+// SOLUTION:
+function squareSum(numbers){
+    let massive = [];
+    for(let i = 0; i < numbers.length; i++) {
+        const double = numbers[i] * numbers[i];
+        massive.push(double);
+    }
+    let sum = 0;
+    for(let n = 0; n < massive.length; n++) {
+        sum = sum + massive[n];
+    }
+    return sum;
+}
