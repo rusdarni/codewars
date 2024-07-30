e685c4080b5334c09ac991236ead275e451b0cf2
dasha <rus.dar.ni@gmail.com>
2024-07-30 10:52:23 +0300

    calculate-average.js

---
+++ 8kyu/calculate-average.js
@@ -0,0 +1,21 @@
+// DESCRIPTION:
+//     Write a function which calculates the average of the numbers in a given list.
+//
+//     Note: Empty arrays should return 0.
+
+// SOLUTION:
+
+function findAverage(array) {
+    if (array.length < 1) {
+        return 0;
+    }
+
+    let average = 0;
+    for(let i = 0; i < array.length; i++) {
+        average += array[i];
+    }
+    average = average / array.length;
+    return average;
+}
+
+console.log("findAverage: ", findAverage(1,2,1))
