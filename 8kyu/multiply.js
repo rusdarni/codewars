8c059d7b1f2a9cc0bc28ac9e98c85221798294b5
dasha <rus.dar.ni@gmail.com>
2025-11-03 23:56:23 +0300

    multiply.js

--- 8kyu/multiply.js
+++ 8kyu/multiply.js
@@ -1,10 +1,18 @@
-// DESCRIPTION:
-//    This code does not execute properly. Try to figure out why.
+// 🐱‍👤DESCRIPTION
+// This code does not execute properly. Try to figure out why.
 
+// 🐱‍💻TESTS
+// assert.strictEqual(multiply(1,1), 1);
+// assert.strictEqual(multiply(2,1), 2);
+// assert.strictEqual(multiply(2,2), 4);
+// assert.strictEqual(multiply(3,5), 15);
+// assert.strictEqual(multiply(5,0), 0);
+// assert.strictEqual(multiply(0,5), 0);
+// assert.strictEqual(multiply(0,0), 0);
 
-// SOLUTION:
+// 🍀SOLUTION
 multiply = function (a, b) {
     return a * b;
 }
 
-console.log("multiply: ", multiply(2, 3))
+console.log("multiply: ", multiply(0,5))
