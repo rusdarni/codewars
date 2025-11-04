391105fbb472a1f66de4b7b670dc6a7c8be46386
dasha <rus.dar.ni@gmail.com>
2025-11-04 20:21:05 +0300

    convert-boolean-values-to-string

---
+++ 8kyu/convert-boolean-values-to-strings.js
@@ -0,0 +1,11 @@
+// 🐱‍👤DESCRIPTION
+// Complete the method that takes a boolean value and return a "Yes" string for true, or a "No" string for false.
+
+// 🐱‍💻TESTS
+// assert.strictEqual(boolToWord(true), 'Yes')
+// assert.strictEqual(boolToWord(false), 'No')
+
+//🍀SOLUTION
+function boolToWord( bool ){
+    return bool ? "Yes" : "No"
+}
